b665731ae25801dfd5ba3b72992a6cc5b18741d6
Murilo Thom <murilothom.2002@hotmail.com>
2024-01-19 10:10:11 -0300

    Initial Commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  const port = Number(process.env.PORT);
+  await app.listen(port);
+  console.log('******************************');
+  console.log(`        SERVER RUNNING        `);
+  console.log(`    Listening on port ${port} `);
+  console.log('******************************');
+}
+bootstrap();
